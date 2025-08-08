329c077a7086ddc9de4c042e3a585b6ed76a20a9
rm-srpark <parksora@ip-192-168-249-41.ap-northeast-2.compute.internal>
2025-08-08 15:40:46 +0900

    Todo 전체 조회 테스트 코드 작성

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "모든 Todo를 조회한다" do
+    todos = Todo.all
+
+    puts "조회된 Todo 개수: #{todos.count}"
+    todos.each do |todo|
+      puts "Todo ID: #{todo.id}, 제목: #{todo.title}, 생성일자: #{todo.created_by}"
+    end
+
+    expect(todos).to be_a(ActiveRecord::Relation)
+  end
 end
