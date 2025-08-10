ed005c7911f1a3b04f80ee896d1e2170db114b46
rm-srpark <parksora@ip-192-168-249-41.ap-northeast-2.compute.internal>
2025-08-10 15:08:45 +0900

    todos_controller 생성 및 라우팅 설정

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,18 @@
 class TodosController < ApplicationController
+  # GET /todos
+  def index
+    todos = Todo.all
+    json_response(todos)
+  end
+
+  # Post /todos
+  def create
+    todo = Todo.create!(todo_params)
+    json_response(todo, :created)
+  end
+
+  private
+  def todo_params
+    params.permit(:title, :created_by)
+  end
 end
